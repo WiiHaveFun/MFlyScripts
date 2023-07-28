27c286f4600cf846ce02d898828806c1edd10491
michaelchen <jr.michael.chen@gmail.com>
2023-07-28 17:06:16 -0400

    Renaming again

--- Functions/extractData.m
+++ Functions/extractData.m
@@ -1,6 +1,6 @@
 function [rc, st, sf] = extractData(length, path)
-    %ExtractData Parses the stability derivative and strip force files in a
-    %give folder
+    % ExtractData Parses the stability derivative and strip force files in a
+    % given folder
 
     for i = 1:length
         rc{i} = parseRunCaseHeader(strcat(path, "/ST/data", string(i), ".txt"));
---
+++ QM-3/Trade2_bwb_wing_sizing/PADA_Trade2_bwb_wing_sizing.m
@@ -0,0 +1,6 @@
+% Michael Chen - July 28, 2023
+% This script generates a CSV for PADA wing sizing
+% The plane has a rectangular fuselage (constant chord and fixed width)
+% The rest of the wing is swept and tapered
+% The start of the wing, after the fuselage, is a narrow transition region
+% with interpolated airfoils
