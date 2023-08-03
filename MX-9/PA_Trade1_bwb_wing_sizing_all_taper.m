67904d1e4311898a14a010da97a7d059123c5157
michaelchen <jr.michael.chen@gmail.com>
2023-08-03 15:30:17 -0400

    Outlined PA trade 1

---
+++ MX-9/PA_Trade1_bwb_wing_sizing_all_taper.m
@@ -0,0 +1,45 @@
+% Michael Chen - August 3, 2023
+% This script generates a CSV for PA wing sizing
+
+%% Trade 1
+%{
+The plane has a rectangular fuselage (constant chord and fixed width)
+There is a trapezoidal transition region connecting the fuselage and wing
+The wing is rectangular/tapered/partially tapered.
+
+This version is for fully tapered wings (i.e. taper starts at wing start).
+
+Constants are
+- wingspan (9.95')
+- wing chord (2')
+- fuselage area (1000 in^3)
+
+Parameters to modify are
+- fuselage length (3'-5')
+- taper position ratio (0-1.0)
+- taper ratio (0.2-1.0)
+- transition width (3"-6") (6"-12" for both sides)
+- transition setback ratio (0-1.0)
+
+Output
+- sref, cref, bref
+- tip chord
+- tip position (y)
+- leading edge position (x)
+- transition end/wing start position (y)
+- fuselage chord
+- fuselage end position/transition start position (y)
+%}
+
+useMeters = false;
+
+% In inches
+wingspan = 9.95 * 12;
+wingChord = 24;
+fuselageArea = 1000; % in^3
+
+fuselageLength = 36:4:60;
+% taperPositionRatio = 0.0; % 1.0 means no taper, 0.0 mean all taper
+taperRatio = 1.0:-0.2:0.2;
+transitionWidth = 3:1:6;
+transitionSetbackRatio = 0:0.2:1.0; % 0.0 means wing at front, 1.0 means wing at back
