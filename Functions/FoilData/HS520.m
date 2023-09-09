325d6b9d4d822c4bd9d270291b67df8da6f4ac1b
michaelchen <jr.michael.chen@gmail.com>
2023-09-09 17:10:58 -0400

    added and updated data about foil max cl

--- Functions/FoilData/HS520.m
+++ Functions/FoilData/HS520.m
@@ -1,10 +1,10 @@
-function pp = ch10sm(plotData)
-    % ch10sm returns a piecewise polynomial for the CH10 airfoil that
+function pp = HS520(plotData)
+    % HS520 returns a piecewise polynomial for the HS520 airfoil that
     % relates max lift coefficient to Reynolds number.
     %
     %   plotData = boolean that plots a graph of the data
     Re = [0.1e6, 0.13e6, 0.16e6, 0.2e6, 0.25e6, 0.275e6, 0.3e6, 0.35e6, 0.4e6, 0.45e6, 0.5e6, 0.625e6, 0.75e6, 0.875e6, 1e6];
-    MaxCl = [1.6781, 1.7873, 1.8888, 1.9552, 2.0094, 2.0193, 2.0260, 2.0285, 2.0261, 2.0267, 2.0291, 2.0358, 2.0409, 2.0411, 2.0433];
+    MaxCl = [1.1403, 1.1722, 1.1679, 1.1929, 1.2028, 1.1989, 1.2208, 1.2516, 1.2639, 1.2695, 1.2861, 1.3513, 1.3941, 1.4278, 1.4447];
 
     pp = spline(Re, MaxCl);
 
--- Functions/FoilData/NACA23012.m
+++ Functions/FoilData/NACA23012.m
@@ -1,10 +1,10 @@
-function pp = ch10sm(plotData)
-    % ch10sm returns a piecewise polynomial for the CH10 airfoil that
+function pp = NACA23012(plotData)
+    % NACA23012 returns a piecewise polynomial for the NACA 23012 airfoil that
     % relates max lift coefficient to Reynolds number.
     %
     %   plotData = boolean that plots a graph of the data
     Re = [0.1e6, 0.13e6, 0.16e6, 0.2e6, 0.25e6, 0.275e6, 0.3e6, 0.35e6, 0.4e6, 0.45e6, 0.5e6, 0.625e6, 0.75e6, 0.875e6, 1e6];
-    MaxCl = [1.6781, 1.7873, 1.8888, 1.9552, 2.0094, 2.0193, 2.0260, 2.0285, 2.0261, 2.0267, 2.0291, 2.0358, 2.0409, 2.0411, 2.0433];
+    MaxCl = [1.1881, 1.2305, 1.2465, 1.2646, 1.2911, 1.3009, 1.3085, 1.3327, 1.3498, 1.3627, 1.3859, 1.4164, 1.4366, 1.4696, 1.4877];
 
     pp = spline(Re, MaxCl);
 
