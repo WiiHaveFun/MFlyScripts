d32f632bdeb17b99199c2dd57f94ae37ceb77c87
michaelchen <jr.michael.chen@gmail.com>
2023-09-14 22:56:51 -0400

    QM-3C blended wing smaller wingspan

---
+++ QM-3/Trade3_bwb_wing_sizing/Analysis_Trade3.m
@@ -0,0 +1,138 @@
+% Michael Chen - July 30, 2023
+% This script generates plots to analyze Trade 2 for QM-3
+
+%% Read data
+n1 = 180;
+n2 = 4;
+
+[rc, st, ~] = extractData(n1, "/Users/michaelchen/Documents/M-Fly/2023/QM-3/PADA_Trade3/13", true, false);
+rc = cell2mat(rc);
+st = cell2mat(st);
+
+%% Read CSV
+% Display inches
+% Make sure to convert static margin properly
+parameters = readtable("/Users/michaelchen/Documents/M-Fly/2023/QM-3/PADA_Trade3/QM-3_Trade3_inches.csv");
+parametersMeters = readtable("/Users/michaelchen/Documents/M-Fly/2023/QM-3/PADA_Trade3/QM-3_Trade3_meters.csv");
+
+%% Check for stall
+fuselageClMax = 1.25882;
+wingClMax = 1.19288;
+
+fuselageStall = checkForStall(sf, n1, 1, fuselageClMax);
+wingStall = checkForStall(sf, n1, 5, wingClMax);
+
+%% Plot AoA
+AoA = [rc.alpha];
+
+plot4D(parameters.sweepAngle, parameters.bref, parameters.taperRatio, parameters.transitionWidth, AoA, n1, n2, ...
+    "Sweep angle", "Wing span", "Taper ratio", "Transition width", "AoA");
+
+%% Plot AoA < 8
+AoA = [rc.alpha];
+AoA(AoA >= 8) = NaN;
+
+plot4D(parameters.sweepAngle, parameters.bref, parameters.taperRatio, parameters.transitionWidth, AoA, n1, n2, ...
+    "Sweep angle", "Wing span", "Taper ratio", "Transition width", "AoA");
+
+%% Plot CL/CD with AoA < 8 and statically stable
+AoA = [rc.alpha];
+Cma = [st.Cma];
+
+CL = [rc.CLtot];
+CD = [rc.CDff];
+LD = CL ./ CD;
+
+LD(AoA >= 8 | Cma >= 0) = NaN;
+
+plot4D(parameters.sweepAngle, parameters.bref, parameters.taperRatio, parameters.transitionWidth, LD, n1, n2, ...
+    "Sweep angle", "Wing span", "Taper ratio", "Transition width", "L/D");
+
+%% Plot static margin with AoA < 8 and statically stable
+AoA = [rc.alpha];
+Cma = [st.Cma];
+
+crefMeters = parameters.cref ./ 39.37;
+x_cg = 0.1092; % 4.3 in 
+
+staticMargin = ([st.NP] - x_cg) ./ crefMeters' .* 100;
+
+staticMargin(AoA >= 8 | Cma >= 0) = NaN;
+
+plot4D(parameters.sweepAngle, parameters.bref, parameters.taperRatio, parameters.transitionWidth, staticMargin, n1, n2, ...
+    "Sweep angle", "Wing span", "Taper ratio", "Transition width", "Static margin");
+
+%% Plot L/D with AoA < 8, statically stable, and with static margin between 10-15%
+AoA = [rc.alpha];
+Cma = [st.Cma];
+
+CL = [rc.CLtot];
+CD = [rc.CDff];
+LD = CL ./ CD;
+
+crefMeters = parameters.cref ./ 39.37;
+x_cg = 0.1092; % 4.3 in 
+
+staticMargin = ([st.NP] - x_cg) ./ crefMeters' .* 100;
+
+LD(AoA >= 8 | Cma >= 0 | staticMargin < 10 | staticMargin > 15) = NaN;
+
+plot4D(parameters.sweepAngle, parameters.bref, parameters.taperRatio, parameters.transitionWidth, LD, n1, n2, ...
+    "Sweep angle", "Wing span", "Taper ratio", "Transition width", "L/D");
+
+%% Plot static margin with AoA < 8, statically stable, with reduced wing span 24in
+AoA = [rc.alpha];
+Cma = [st.Cma];
+
+CL = [rc.CLtot];
+CD = [rc.CDff];
+LD = CL ./ CD;
+
+crefMeters = parameters.cref ./ 39.37;
+x_cg = 0.1092; % 4.3 in 
+
+staticMargin = ([st.NP] - x_cg) ./ crefMeters' .* 100;
+
+wingSpan = parameters.bref';
+
+LD(AoA >= 8 | Cma >= 0 | staticMargin < 10 | staticMargin > 15 | wingSpan ~= 24) = NaN;
+
+plot4D(parameters.sweepAngle, parameters.bref, parameters.taperRatio, parameters.transitionWidth, LD, n1, n2, ...
+    "Sweep angle", "Wing span", "Taper ratio", "Transition width", "L/D");
+
+%% Get max L/D data for constraints
+AoA = [rc.alpha];
+Cma = [st.Cma];
+
+CL = [rc.CLtot];
+CD = [rc.CDff];
+LD = CL ./ CD;
+
+crefMeters = parameters.cref ./ 39.37;
+x_cg = 0.1092; % 4.3 in 
+
+staticMargin = ([st.NP] - x_cg) ./ crefMeters' .* 100;
+
+wingSpan = parameters.bref';
+
+% LD(AoA >= 8 | Cma >= 0 | staticMargin < 10 | staticMargin > 15) = NaN;
+% LD(AoA >= 8 | Cma >= 0 | staticMargin < 5 | wingSpan ~= 28) = NaN;
+LD(AoA >= 8 | Cma >= 0 | staticMargin < 10 | staticMargin > 15 | wingSpan ~= 24) = NaN;
+
+runCase = find(LD == max(LD));
+
+CM = [rc.Cmtot];
+e = [rc.e];
+Xnp = [st.NP];
+
+disp(strcat("CL = ", string(CL(runCase))));
+disp(strcat("CD = ", string(CD(runCase))));
+disp(strcat("CL/CD = ", string(LD(runCase))));
+disp(strcat("CM = ", string(CM(runCase))));
+disp(strcat("e = ", string(e(runCase))));
+disp(strcat("Xnp = ", string(Xnp(runCase))));
+disp(strcat("static margin = ", string(staticMargin(runCase))));
+disp(strcat("alpha = ", string(AoA(runCase))));
+
+disp(parameters(runCase, :));
+disp(parametersMeters(runCase, :));
---
+++ QM-3/Trade3_bwb_wing_sizing/PADA_Trade3_bwb_wing_sizing.m
@@ -0,0 +1,113 @@
+% Michael Chen - Sep 14, 2023
+% This script generates a CSV for PADA wing sizing
+
+%% Trade 2
+%{
+The plane has a rectangular fuselage (constant chord and fixed width)
+The rest of the wing is swept and tapered
+The start of the wing, after the fuselage, is a narrow transition region
+with interpolated airfoils
+
+Constants are
+- wing/fuselage root chord (15")
+- fuselage width (4") (includes both halves)
+
+Parameters to modify are
+- wing span (20"-28")
+- taper ratio (0.2-1.0)
+- sweep angle (20-30 deg)
+- transition width (0.5"-2") (1"-4" for both sides)
+
+Output
+- sref, cref, bref
+- tip chord
+- tip position (x, y)
+- transition end/wing start chord
+- transition end/wing start position (x, y)
+%}
+
+useMeters = true;
+
+% In inches
+rootChord = 15;
+fuselageWidth = 4;
+
+wingSpan = 20:4:28;
+taperRatio = 1.0:-0.2:0.2;
+sweepAngle = 20:5:30;
+transitionWidth = 0.5:0.5:2;
+
+if useMeters
+    rootChord = rootChord / 39.37;
+    fuselageWidth = fuselageWidth / 39.37;
+    wingSpan = wingSpan / 39.37;
+    transitionWidth = transitionWidth / 39.37;
+end
+
+numRunCases = length(wingSpan) * length(taperRatio) * length(sweepAngle) * length(transitionWidth);
+disp(strcat("Num run cases = ", string(numRunCases)));
+
+[wingSpan, taperRatio, sweepAngle, transitionWidth] = ndgrid(wingSpan, taperRatio, sweepAngle, transitionWidth);
+
+% Tip chord from definition of taper ratio
+tipChord = taperRatio .* rootChord;
+
+% Wing area is trapezoidal plus fuselage rectangle
+% sref = wingSpan .* (rootChord + tipChord) ./ 2;
+wingArea = (wingSpan - fuselageWidth) .* (rootChord + tipChord) ./ 2;
+fuselageArea = fuselageWidth * rootChord;
+sref = wingArea + fuselageArea;
+
+% Wing span
+bref = wingSpan;
+
+% MAC is integrated
+syms y rootCh tipCh fuselageEnd halfSpan S
+
+% Define chord as a piecewise linear function
+c(y, rootCh, tipCh, fuselageEnd, halfSpan) = piecewise(0 <= y & y < fuselageEnd, ...
+    rootCh, ...
+    fuselageEnd <= y & y <= halfSpan, ...
+    rootCh - (rootCh - tipCh) / (halfSpan - fuselageEnd) * (y - fuselageEnd));
+
+% Integral for calculating mean aerodynamic chord
+MAC(rootCh, tipCh, fuselageEnd, halfSpan, S) = 2 / S * int(c(y, rootCh, tipCh, fuselageEnd, halfSpan)^2, y, 0, halfSpan);
+
+% Get output from symbolic function
+cref = double(vpa(MAC(rootChord, tipChord, fuselageWidth / 2, wingSpan / 2, sref)));
+
+% Tip position y is half span
+tipPositionY = wingSpan / 2;
+
+% Tip position x is calculated from sweep angle about the quarter-chord
+% line.
+tipPositionX = (tipPositionY - fuselageWidth / 2) .* tan(deg2rad(sweepAngle)) + rootChord / 4 - tipChord / 4;
+
+% Transition end position y
+transitionEndPositionY = fuselageWidth / 2 + transitionWidth;
+
+% Transition end chord is found with linear function
+transitionEndChord = rootChord - (rootChord - tipChord) ./ (tipPositionY - fuselageWidth / 2) .* transitionWidth;
+
+% Transition end position x is found using linear function
+transitionEndPositionX = tipPositionX ./ (tipPositionY - fuselageWidth / 2) .* transitionWidth;
+
+% Reshape the matrices into column vectors
+sref = reshape(sref, [], 1);
+cref = reshape(cref, [], 1);
+bref = reshape(bref, [], 1);
+tipChord = reshape(tipChord, [], 1);
+tipPositionX = reshape(tipPositionX, [], 1);
+tipPositionY = reshape(tipPositionY, [], 1);
+transitionEndChord = reshape(transitionEndChord, [], 1);
+transitionEndPositionX = reshape(transitionEndPositionX, [], 1);
+transitionEndPositionY = reshape(transitionEndPositionY, [], 1);
+taperRatio = reshape(taperRatio, [], 1);
+sweepAngle = reshape(sweepAngle, [], 1);
+transitionWidth = reshape(transitionWidth, [], 1);
+
+% Output a CSV
+output = round([sref, cref, bref, tipChord, tipPositionX, tipPositionY, transitionEndChord, transitionEndPositionX, transitionEndPositionY, taperRatio, sweepAngle, transitionWidth], 4);
+T = array2table(output);
+T.Properties.VariableNames = ["sref", "cref", "bref", "tipChord", "tipPositionX", "tipPositionY", "transitionEndChord", "transitionEndPositionX", "transitionEndPositionY", "taperRatio", "sweepAngle", "transitionWidth"];
+writetable(T, 'QM-3_Trade3.csv');
