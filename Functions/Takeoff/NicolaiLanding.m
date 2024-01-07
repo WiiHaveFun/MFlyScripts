cfb00b09184b5d93e76c5976ae1a63ff72542461
michaelchen <jr.michael.chen@gmail.com>
2024-01-07 16:07:41 -0500

    Add landing approach

--- Functions/Takeoff/NicolaiLanding.m
+++ Functions/Takeoff/NicolaiLanding.m
@@ -29,6 +29,16 @@ t = 0; % time elapsed (s)
 
 dt = 0.01; % (s)
 
+%% Approach
+% From 50 feet
+
+% theta_approach = deg2rad(9);
+% 
+% S_A = 15.24 / tan(theta_approach);
+
+V50 = 1.3 * VStall;
+
+S_A = W / D(VTD, CD_Ground) * ((V50^2 - VTD^2) / (2 * g) + 15.24);
 %% Takeoff run
 % Iterate from standstill, to takeoff velocity
 % Early rotation when lift is greater than weight
@@ -49,8 +59,8 @@ S_G = S;
 
 %% Plots
 
-x = [0, S_G, ];
-y = [0, 0];
+x = [0, S_A, S_A+S_G];
+y = [15.24, 0, 0];
 
 plot(x*39.37/12, y*39.37/12, LineWidth=3);
 hold on
